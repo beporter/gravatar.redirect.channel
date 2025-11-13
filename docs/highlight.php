b5a24be91f25203b3c0c79ebe58721355953e297
Brian Porter <beporter@users.sourceforge.net>
2025-11-13 11:01:51 -0600

    I was wrong.

--- docs/highlight.php
+++ docs/highlight.php
@@ -2,10 +2,10 @@
 /*
  * Apache only supports 'automatic' syntax highlighting of .phps files via the
  * `mod_php` extension. If a hosting provider is running PHP through FPM or
- * FastCGI, the `.htaccess` approach of adding
+ * FastCGI, the `.htaccess` approach of simply adding
  * `RewriteRule ^(.+\.php)s$ $1 [H=application/x-httpd-php-source]` doesn't
- * work. The workaround is to have another PHP script act as the syntax h
- * ighlighter for a requested .phps file.
+ * work. The workaround is to have another PHP script act as the syntax
+ * highlighter for a requested .phps file.
  *
  * This opens up **ALL KINDS** of nasty security vulnerabilities if done
  * incorrectly, because the highlighter script could be enticed to try
@@ -15,10 +15,13 @@
  * is WAY more elegant though, if you ask me.)
  */
 
-// As long as this file is named the same as the file you want to highlight
-// (just with an added trailing 's'), it can be dropped in anywhere without
-// further modification.
-highlight_file(basename(__FILE__), 's');
+// Make sure the browser renders the result instead of downloading it.
+header('Content-Type: text/html');
+
+// We get a `$_GET['file'] param from the `.htaccess` redirect, but using
+// it is fraught with danger, so hardcode the source `.php` file we want to
+// highlight and ignore the GET param entirely.
+highlight_file('./gravatarize.php');
 
 // Not needed, but it doesn't hurt to make it explicit that this script
 // _shouldn't_ try to do anything else.
